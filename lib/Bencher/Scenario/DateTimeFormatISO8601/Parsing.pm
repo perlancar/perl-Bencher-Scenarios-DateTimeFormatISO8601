4ec6a463069eda8af316d7d3e2829a5468240007
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-07-01 08:37:09 +0700

    Release v0.001

--- lib/Bencher/Scenario/DateTimeFormatISO8601/Parsing.pm
+++ lib/Bencher/Scenario/DateTimeFormatISO8601/Parsing.pm
@@ -13,19 +13,23 @@ our $scenario = {
         {
             name => 'parse_datetime',
             module => 'DateTime::Format::ISO8601',
-            code_template => 'for (1..10_000) { DateTime::Format::ISO8601->parse_datetime(<str>) }',
+            code_template => 'DateTime::Format::ISO8601->parse_datetime(<str>)',
             tags => ['parse_datetime'],
         },
         {
             name => 'parse_time',
             module => 'DateTime::Format::ISO8601',
-            code_template => 'for (1..10_000) { DateTime::Format::ISO8601->parse_time(<str>) }',
+            code_template => 'DateTime::Format::ISO8601->parse_time(<str>)',
             tags => ['parse_time'],
         },
     ],
     datasets => [
-        {include_participant_tags => ['parse_datetime'], args => {'str@' => ['2000-12-31', '2000-12-31T12:34:56', '2000-12-31T12:34:56Z', '2000-12-31T12:34:56+0700']}},
-        {include_participant_tags => ['parse_time'    ], args => {'str@' => ['12:34:56', '12:34:56Z', '12:34:56+0700']}},
+        {include_participant_tags => ['parse_datetime'], args => {'str@' => ['2000-12-31', '2000-12-31T12:34:56', '2000-12-31T12:34:56Z', '2000-12-31T12:34:56+07:00']}},
+        #{include_participant_tags => ['parse_time'    ], args => {'str@' => [
+            #'12:34:56',
+            #'12:34:56Z',
+            #'12:34:56+07:00',
+        #]}},
     ],
 };
 
--- lib/Bencher/Scenarios/DateTimeFormatISO8601.pm
+++ lib/Bencher/Scenarios/DateTimeFormatISO8601.pm
@@ -5,3 +5,17 @@ package Bencher::Scenarios::DateTimeFormatISO8601;
 
 1;
 # ABSTRACT: Scenarios to benchmark DateTime::Format::ISO8601
+
+=head1 DESCRIPTION
+
+This distribution contains L<Bencher> scenarios to benchmark
+L<DateTime::Format::ISO8601>.
+
+
+=head1 SEE ALSO
+
+L<Bencher::Scenarios::DateTimeFormatISO8601Format>, scenarios to benchmark
+L<DateTime::Format::ISO8601::Format>.
+
+L<Bencher::Scenarios::DateTimeFormatDurationISO8601>, scenarios to benchmark
+L<DateTime::Format::Duration::ISO8601>.
